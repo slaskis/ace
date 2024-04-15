c91db3253274a12de1a9e098ca4dae327c734db0
Robert Sköld <robert@publicclass.se>
2024-04-15 08:49:02 +0200

    Add test for multiple recipients/identities

--- main_test.go
+++ main_test.go
@@ -95,6 +95,7 @@ func TestAce(t *testing.T) {
 			test.Snapshot(t, buf.Bytes())
 		})
 	})
+
 	t.Run("env", func(t *testing.T) {
 		os.Remove("testdata/.env3.ace")
 		{
@@ -119,6 +120,71 @@ func TestAce(t *testing.T) {
 			buf := &bytes.Buffer{}
 			output = buf
 			cmd := &Env{EnvFile: "testdata/.env3.ace", Identities: argp.Append{I: &([]string{"testdata/identity2"})}, Command: []string{"sh", "-c", "echo $A $B $C"}}
+			err := cmd.Run()
+			if err != nil {
+				t.Fatal(err)
+			}
+			test.Snapshot(t, buf.Bytes())
+		})
+	})
+
+	t.Run("multiple recipients repeated flags", func(t *testing.T) {
+		os.Remove("testdata/.env4.ace")
+		{
+			cmd := &Set{EnvFile: "testdata/.env4.ace", Recipients: argp.Append{I: &([]string{"testdata/recipients1.txt", "testdata/recipients2.txt"})}, EnvPairs: []string{"A=1", "B=2", "C=1 2 3 "}}
+			err := cmd.Run()
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+		{
+			cmd := &Set{EnvFile: "testdata/.env4.ace", Recipients: argp.Append{I: &([]string{"testdata/recipients1.txt"})}, EnvPairs: []string{"A=2", "D=3"}}
+			err := cmd.Run()
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+		{
+			cmd := &Set{EnvFile: "testdata/.env4.ace", Recipients: argp.Append{I: &([]string{"testdata/recipients2.txt"})}, EnvPairs: []string{"C=333 "}}
+			err := cmd.Run()
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+		t.Run("identity1", func(t *testing.T) {
+			buf := &bytes.Buffer{}
+			output = buf
+			cmd := &Get{EnvFile: "testdata/.env4.ace", Identities: argp.Append{I: &([]string{"testdata/identity1"})}}
+			err := cmd.Run()
+			if err != nil {
+				t.Fatal(err)
+			}
+			test.Snapshot(t, buf.Bytes())
+		})
+		t.Run("identity2", func(t *testing.T) {
+			buf := &bytes.Buffer{}
+			output = buf
+			cmd := &Get{EnvFile: "testdata/.env4.ace", Identities: argp.Append{I: &([]string{"testdata/identity2"})}}
+			err := cmd.Run()
+			if err != nil {
+				t.Fatal(err)
+			}
+			test.Snapshot(t, buf.Bytes())
+		})
+		t.Run("identity1,identity2", func(t *testing.T) {
+			buf := &bytes.Buffer{}
+			output = buf
+			cmd := &Get{EnvFile: "testdata/.env4.ace", Identities: argp.Append{I: &([]string{"testdata/identity1", "testdata/identity2"})}}
+			err := cmd.Run()
+			if err != nil {
+				t.Fatal(err)
+			}
+			test.Snapshot(t, buf.Bytes())
+		})
+		t.Run("identity2,identity1", func(t *testing.T) {
+			buf := &bytes.Buffer{}
+			output = buf
+			cmd := &Get{EnvFile: "testdata/.env4.ace", Identities: argp.Append{I: &([]string{"testdata/identity2", "testdata/identity1"})}}
 			err := cmd.Run()
 			if err != nil {
 				t.Fatal(err)
