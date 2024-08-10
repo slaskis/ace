424f6a2840191b0e55117a36bbff6d01f7c855cd
Robert Sköld <robert@publicclass.se>
2024-08-10 10:02:23 +0200

    Refactor to improve coverage

--- env.go
+++ env.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"os/exec"
 
-	"filippo.io/age"
 	"github.com/tdewolff/argp"
 )
 
@@ -40,50 +39,9 @@ func (cmd *Env) Run() error {
 		defer src.Close()
 	}
 
-	if _, exists := os.LookupEnv("XDG_CONFIG_HOME"); !exists {
-		dir, err := os.UserConfigDir()
-		if err != nil {
-			return fmt.Errorf("unable to read user config dir: %w", err)
-		}
-		os.Setenv("XDG_CONFIG_HOME", dir)
-	}
-
-	idents := *cmd.Identities.I.(*[]string)
-	if len(idents) == 0 {
-		idents = []string{"$XDG_CONFIG_HOME/ace/identity"}
-	}
-
-	var identities []age.Identity
-	for _, id := range idents {
-		err := func() error {
-			i, err := os.Open(os.ExpandEnv(id))
-			if err != nil {
-				if errors.Is(err, os.ErrNotExist) {
-					switch cmd.OnMissing {
-					case "ignore":
-						return nil
-					case "warn", "warning":
-						slog.Warn("identity not found")
-						return nil
-					default:
-						return err
-					}
-				} else {
-					return err
-				}
-			}
-			defer i.Close()
-
-			idents, err := age.ParseIdentities(i)
-			if err != nil {
-				return err
-			}
-			identities = append(identities, idents...)
-			return nil
-		}()
-		if err != nil {
-			return err
-		}
+	identities, err := readIdentities(*cmd.Identities.I.(*[]string), cmd.OnMissing)
+	if err != nil {
+		return err
 	}
 
 	vars, err := readEnvFile(src, identities)
--- get.go
+++ get.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"strings"
 
-	"filippo.io/age"
 	"github.com/tdewolff/argp"
 )
 
@@ -22,38 +21,9 @@ func (cmd *Get) Run() error {
 	}
 	defer src.Close()
 
-	if _, exists := os.LookupEnv("XDG_CONFIG_HOME"); !exists {
-		dir, err := os.UserConfigDir()
-		if err != nil {
-			return err
-		}
-		os.Setenv("XDG_CONFIG_HOME", dir)
-	}
-
-	idents := *cmd.Identities.I.(*[]string)
-	if len(idents) == 0 {
-		idents = []string{"$XDG_CONFIG_HOME/ace/identity"}
-	}
-
-	var identities []age.Identity
-	for _, id := range idents {
-		err := func() error {
-			i, err := os.Open(os.ExpandEnv(id))
-			if err != nil {
-				return err
-			}
-			defer i.Close()
-
-			idents, err := age.ParseIdentities(i)
-			if err != nil {
-				return err
-			}
-			identities = append(identities, idents...)
-			return nil
-		}()
-		if err != nil {
-			return err
-		}
+	identities, err := readIdentities(*cmd.Identities.I.(*[]string), "error")
+	if err != nil {
+		return err
 	}
 
 	vars, err := readEnvFile(src, identities)
